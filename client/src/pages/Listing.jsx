c96fb7802d1edeac91fbba8009d92b09fecc739d
Niraj-Kumar-Singh <nirajsingh3120@gmail.com>
2024-02-26 02:33:49 +0530

    Added contact landlord functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {Swiper, SwiperSlide} from 'swiper/react';
 import SwiperCore from 'swiper';
+import {useSelector} from 'react-redux';
 import {Navigation} from 'swiper/modules';
 import 'swiper/css/bundle';
 import {
@@ -13,6 +14,7 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import Contact from "../components/Contact";
 
 
 function Listing() {
@@ -21,7 +23,9 @@ function Listing() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
   const params = useParams();
+  const {currentUser} = useSelector((state) => state.user);
   useEffect(() => {
     const fetchListing = async () => {
       try {
@@ -127,7 +131,7 @@ function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            {/* 
+            
             {currentUser && listing.userRef !== currentUser._id && !contact && (
               <button
                 onClick={() => setContact(true)}
@@ -135,8 +139,8 @@ function Listing() {
               >
                 Contact landlord
               </button>
-            )}
-            {contact && <Contact listing={listing} />} */}
+             )}
+            {contact && <Contact listing={listing} />}
           </div>
 
         </div>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -265,7 +265,7 @@ export default function Profile() {
               key={listing._id}
               className="border rounded-lg p-3 flex justify-between items-center gap-4"
             >
-              <Link to={`/listings/${listing._id}`}>
+              <Link to={`/listing/${listing._id}`}>
                 <img
                   src={listing.imageUrls[0]}
                   alt="listings cover"
@@ -274,7 +274,7 @@ export default function Profile() {
               </Link>
               <Link
                 className="flex-1 text-slate-700 font-semibold hover:underline truncate"
-                to={`/listings/${listing._id}`}
+                to={`/listing/${listing._id}`}
               >
                 <p>{listing.name}</p>
               </Link>
